861e8bbbd06fe64007d6b64b39b9f9ca81b5996e
Morten Engemand <engemand@hotmail.com>
2018-05-15 14:26:53 +0200

    syng beregnarray

--- BeregnArray/Program.cs
+++ BeregnArray/Program.cs
@@ -8,8 +8,21 @@ namespace BeregnArray
 {
     class Program
     {
+        static returtype BeregnOgSorterArray(int[] input)
+        {
+            returtype r2 = new returtype();
+
+            return r2;
+        }
+
+
         static void Main(string[] args)
         {
         }
+    }
+    struct returtype
+    {
+        public double sum;
+        public double gennemsnit;
     }
 }
