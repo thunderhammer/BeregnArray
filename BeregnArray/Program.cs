61f1ab3aa0365a8bf53d4f2d285896c5d5a43cb8
Morten Engemand <engemand@hotmail.com>
2018-05-15 14:02:26 +0200

    Add project files.

---
+++ BeregnArray/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeregnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
