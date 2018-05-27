e2c49895d8ce6388332e16083f9fd44a5e31e41e
Morten Engemand <engemand@gmail.com>
2018-05-27 23:38:36 +0200

    Stacking and heaping

--- BeregnArray/Program.cs
+++ BeregnArray/Program.cs
@@ -11,6 +11,18 @@ namespace BeregnArray
         static returtype BeregnOgSorterArray(int[] input)
         {
             returtype r2 = new returtype();
+            /*r2.sum = input.Sum();*/
+            foreach (var number in input)
+            {
+                r2.sum += number;
+            }
+
+            r2.gennemsnit = Math.Round(r2.sum / input.Count(),2);
+            Array.Sort(input);
+            foreach (var item in input)
+            {
+                Console.WriteLine(item.ToString());
+            }
 
             return r2;
         }
@@ -18,6 +30,17 @@ namespace BeregnArray
 
         static void Main(string[] args)
         {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSorterArray(test);
+            Console.WriteLine(res.sum);
+            Console.WriteLine(res.gennemsnit);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+
         }
     }
     struct returtype
