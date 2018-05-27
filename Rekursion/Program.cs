a5d87597d66e38ed703e0e442acda57faa777133
Morten Engemand <engemand@gmail.com>
2018-05-28 00:24:50 +0200

    Rekursion1

---
+++ Rekursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekursion
+{
+    class Program
+    {
+        static void RecursiveWrite(int number, int stop)
+        {
+            Console.WriteLine(number);
+            if (number == stop)
+                return;
+            else
+            {
+                number++;
+                RecursiveWrite(number, stop);
+            }
+
+        } 
+        static void Main(string[] args)
+        {
+            RecursiveWrite(1, 10);
+        }
+    }
+}
